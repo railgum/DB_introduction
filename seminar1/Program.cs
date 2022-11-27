0a0afc6b24274f663fc857fd2eef4d6c18aaf6ae
Rail <railbolo@gmail.com>
2022-11-27 20:51:41 +0400

    end seminar1

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,20 +1,21 @@
 ﻿/* Создание и вывод таблицы в файл (кодировка!)
-
+*/
 string result = String.Empty;
 
 string[] marks = { "троечник", "ударник", "отличник" };
+string[] fnames = { "Сергей", "Дмитрий", "Ирина", "Алиса" };
+string[] lnames = { "Петров(-а)", "Баранов(-а)", "Ленин(-а)", "Гаврилов(-а)" };
+
 for (int i = 0; i < 100; i++)
 {
-  int id = i;
-  string fname = $"Имя_{i}";
-  string lname = $"Фамилия_{i}";
+  string fname = fnames[new Random().Next(fnames.Length)];
+  string lname = lnames[new Random().Next(lnames.Length)];
   string mark = marks[new Random().Next(marks.Length)];
-  result += $"{id}; {fname}; {lname}; {mark};\n";
+  result += $"{i + 1}; {fname}; {lname}; {mark};\n";
 }
 //Console.WriteLine($"{result}");
 File.WriteAllText("db.csv", result);
 
-*/
 
 /* Через draw.io
   Table_УЧЕНИКИ
