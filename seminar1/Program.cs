3caa5b3b2b98b59e607f8e24752adff175446246
Rail <railbolo@gmail.com>
2022-11-27 19:46:10 +0400

    add struct.drawio.png

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,13 +1,26 @@
-﻿string result = String.Empty;
+﻿/* Создание и вывод таблицы в файл (кодировка!)
+
+string result = String.Empty;
 
 string[] marks = { "троечник", "ударник", "отличник" };
 for (int i = 0; i < 100; i++)
 {
+  int id = i;
   string fname = $"Имя_{i}";
   string lname = $"Фамилия_{i}";
-  int id = i;
   string mark = marks[new Random().Next(marks.Length)];
   result += $"{id}; {fname}; {lname}; {mark};\n";
 }
 //Console.WriteLine($"{result}");
 File.WriteAllText("db.csv", result);
+
+*/
+
+/* Через draw.io
+  Table_УЧЕНИКИ
+    id
+    fname
+    lname
+    mark
+*/
+
